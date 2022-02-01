9ef201675917df861e45cfeef2d56548ed82e1e0
jbawt <jbawtinheimer@gmail.com>
2022-01-31 20:16:44 -0700

    add note labels to db

---
+++ db/seeds/notes/labels.sql
@@ -0,0 +1,5 @@
+INSERT INTO note_labels (user_id, name, handle) VALUES (1, 'Family', 'family');
+INSERT INTO note_labels (user_id, name, handle) VALUES (1, 'Work', 'work');
+INSERT INTO note_labels (user_id, name, handle) VALUES (1, 'Prior', 'prior');
+INSERT INTO note_labels (user_id, name, handle) VALUES (1, 'Personal', 'personal');
+INSERT INTO note_labels (user_id, name, handle) VALUES (1, 'Friends', 'friends');
