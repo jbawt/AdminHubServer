4dd60d4d3420f9932828004973810ed0bb65ac18
jbawt <jbawtinheimer@gmail.com>
2022-01-31 17:02:12 -0700

    add shortcuts to db

---
+++ db/seeds/settings/shortcuts.sql
@@ -0,0 +1,3 @@
+INSERT INTO shortcuts (user_id, title) VALUES (1, 'calendar');
+INSERT INTO shortcuts (user_id, title) VALUES (1, 'mail');
+INSERT INTO shortcuts (user_id, title) VALUES (1, 'contacts');
--- db/seeds/todos/folders.sql
+++ db/seeds/todos/folders.sql
@@ -1,1 +1,3 @@
 INSERT INTO folders (handle, title, icon) VALUES ('all', 'All', 'view_headline');
+INSERT INTO folders (handle, title, icon) VALUES ('incomplete', 'Incomplete', 'assignment');
+INSERT INTO folders (handle, title, icon) VALUES ('complete', 'Completed', 'check_circle');
