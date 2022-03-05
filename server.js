9add9c9d58e3d385f8d71713d30e388d7593021f
jbawt <jbawtinheimer@gmail.com>
2022-03-05 07:07:20 -0700

    connect google drive

--- server.js
+++ server.js
@@ -31,6 +31,7 @@ const board = require('./lib/routes/scrumBoard/board');
 const cards = require('./lib/routes/scrumBoard/cards');
 const lists = require('./lib/routes/scrumBoard/lists');
 const calendar = require('./lib/routes/calendar/calendar');
+const drive = require('./lib/routes/fileManager/fileManager');
 
 app.use('/api/login', login(db));
 app.use('/api/auth/user', updateUser(db));
@@ -45,6 +46,7 @@ app.use('/api/scrumboard-app', cards(db));
 app.use('/api/scrumboard-app', board(db));
 app.use('/api/scrumboard-app', lists(db));
 app.use('/api/calendar-app', calendar(db));
+app.use('/api/file-manager-app', drive(db));
 
 
 app.listen(PORT, () => {
